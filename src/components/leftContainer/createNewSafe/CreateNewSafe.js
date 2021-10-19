cd50317c808513ec732ef957ddd4ed1ba005bee9
Poonam-Gorai <poonamgorai1807@gmail.com>
2021-10-19 13:48:52 +0530

    deployed

--- src/components/leftContainer/createNewSafe/CreateNewSafe.js
+++ src/components/leftContainer/createNewSafe/CreateNewSafe.js
@@ -1,44 +1,49 @@
-
-import {useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import React from "react";
 import Card from "./card";
-import {deleteItem} from '../../../redux/createSafe/createSafe.action';
-import {reLoadsafe} from'../../../redux/createSafe/createSafe.action';
+import { deleteItem } from "../../../redux/createSafe/createSafe.action";
+import { reLoadsafe } from "../../../redux/createSafe/createSafe.action";
 import { useEffect } from "react";
 import api from "../../../api/api";
 
-
-function CreateNewSafe({ setSelectedSafe, setcurrentIndex,setAddButtonDisable,setsafeListLength,safeData}) {
+function CreateNewSafe({
+  setSelectedSafe,
+  setcurrentIndex,
+  setAddButtonDisable,
+  setsafeListLength,
+  safeData,
+}) {
   //const safeList = useSelector((state) => state.createSafe.safes);
-  
+
   //console.log(safeList);
+  console.log(safeData);
 
   const dispatch = useDispatch();
   setsafeListLength(safeData);
 
   useEffect(() => {
-    if(safeData.length>0){
-      setSelectedSafe(safeData[0])
+    if (safeData?.length > 0) {
+      setSelectedSafe(safeData[0]);
     }
-  }, [safeData])
-  
+  }, [safeData]);
+
   const handelClick = (safe, index) => {
-    setcurrentIndex(index)
+    setcurrentIndex(index);
     setSelectedSafe(safe);
     setAddButtonDisable((prevState) => !prevState);
   };
- const onDelete = (index) => {
-  //dispatch(deleteItem(index));
-  api
-  .delete(`/${index}`)
-  .then(result =>{
-    dispatch(reLoadsafe(false))
-  })
-  .catch((error) => {
-    console.log(error.responce);
-  })
-  
- }
+  const onDelete = (_id) => {
+    //dispatch(deleteItem(index));
+    console.log("id is", _id);
+    api
+      .delete(`/${_id}`)
+      .then((result) => {
+        dispatch(reLoadsafe(false));
+      })
+      .catch((error) => {
+        console.log(error.responce);
+      });
+  };
   return (
     <>
       {safeData.length > 0 &&
@@ -49,8 +54,8 @@ function CreateNewSafe({ setSelectedSafe, setcurrentIndex,setAddButtonDisable,se
               handelClick={handelClick}
               safe={safe}
               index={index}
-              onDelete={onDelete}
-              />
+              onDelete={() => onDelete(safe._id)}
+            />
           );
         })}
     </>
